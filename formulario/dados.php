98a7f15bfe0f4e6067adb3bf3c7fe221e9e8534b
vitoria f <freitasvitoria712@gmail.com>
2023-01-25 19:47:41 -0300

    [add] formulario

---
+++ formulario/dados.php
@@ -0,0 +1,20 @@
+<?php 
+
+$name = $_POST['name'];
+$email = $_POST['email']; 
+
+echo "Seu nome é $name e seu email é $email";
+var_dump($_POST);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do formulário</title>
+</head>
+<body>
+ 
+</body>
+</html>
---
+++ formulario/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP formulário</title>
+</head>
+<body>
+ <form actions="dados.php" method="POST">
+    Name:  <input type="text" name="name" method=""><br>
+    Email: <input type="text" name="email" method=""><br>
+    <input type="submit"  name="Enviar"> 
+</form>
+</body>
+</html>
